e175f48b27d6863f7e54635141dbbe92a6b2ed41
Natalia Senatorova <n-senat@mail.ru>
2022-02-17 17:57:34 +0300

    changed test name in test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_basket_button(browser):
+def test_presence_of_basket_button(browser):
     browser.get(link)
     browser.implicitly_wait(10)
     button = browser.find_element_by_css_selector('#add_to_basket_form > button')
