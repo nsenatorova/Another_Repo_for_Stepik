3c3345cf0bd72d6168415b883413cc179bfa8f5d
Natalia Senatorova <n-senat@mail.ru>
2022-02-17 17:47:02 +0300

    it is done

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+    button = browser.find_element_by_css_selector('#add_to_basket_form > button')
+    time.sleep(5)
+    assert button.text is not None
